1a823a54f7aad32a57179ef016dbca1e1f89ddf8
nikita159821 <132481753+nikita159821@users.noreply.github.com>
2024-12-16 12:35:55 +0500

    Update migrated_document.py

--- API_B2B_Center/endpoints/migrated_document.py
+++ API_B2B_Center/endpoints/migrated_document.py
@@ -1,11 +1,11 @@
-import os
+import logging
 import requests
-from dotenv import load_dotenv
+import os
 import allure
-from API_B2B_Center.test.data import URL
+from api_b2b_center.test.data import URL
 
-# Загрузка переменных окружения из .env файла
-load_dotenv()
+logging.basicConfig(level=logging.DEBUG)
+
 
 class MigratedDocument:
     def __init__(self):
@@ -18,8 +18,8 @@ class MigratedDocument:
             'Authorization': f'Bearer {token}',
             'Content-Type': 'application/json'
         }
-
+        logging.debug(f"Sending request to: {URL} with headers: {headers} and payload: {payload}")
         self.response = requests.post(URL, json=payload, headers=headers)
+        logging.debug(f"Response status: {self.response.status_code}")
+        logging.debug(f"Response body: {self.response.text}")
         return self.response
-
-
